4fb6ce9ee35d0a1143488a9bbffdfa9358d7c9ca
JurisDataNerd <arisantofauzan@gmail.com>
2026-05-23 14:40:55 +0700

    Authoriz

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,4 @@
 export default function auth(req, res, next) {
-  console.log("🔥 AUTH MIDDLEWARE HIT");
   // Accept several possible header names used in clients/tests
   const headerCandidates = ["x-user-id", "user-id", "userid", "user_id", "userId"];
 
@@ -15,7 +14,6 @@ export default function auth(req, res, next) {
   // Some clients accidentally send the literal string 'undefined'
   if (user_id === "undefined") user_id = undefined;
 
-  console.log("AUTH header resolved to:", user_id);
 
   if (!user_id) {
     return res.status(401).json({ error: "Unauthorized" });
